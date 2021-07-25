b9f19b8e64611a228fc911640b66b4b238758f33
nesty-juramir <nathan.nariai@gmail.com>
2021-07-25 20:51:24 -0300

    Desafio E-commerce

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/produtos',
+    baseURL: 'https://my-json-server.typicode.com/nyodinariai/products/produtos'//'http://localhost:8080/produtos',
 })
 
 export default api;
